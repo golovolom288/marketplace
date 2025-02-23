faf2ccf5d51dae93dc914d3a4473b4a496aa03da
Lleanin <Fifashf@yandex.ru>
2025-02-23 15:54:51 +0100

    .

--- main/views.py
+++ main/views.py
@@ -1,7 +1,13 @@
 from django.http import JsonResponse
-from django.shortcuts import render
-# from .models import Quest
+from django.shortcuts import render, get_object_or_404
+from .models import Product
 
 
 def home(request):
-    return render(request, 'home.html')
+    products_list = Product.objects.filter(is_hot_deal=True)
+    return render(request, 'home.html', {'products_list': products_list})
+
+
+def product_detail(request, pk):
+    product_item = get_object_or_404(Product, pk=pk)
+    return render(request, 'news_detail.html', {'news_item': product_item})

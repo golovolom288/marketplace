7d80c5dd5face03abccaae59966e9cf15dec8395
golovolom288 <142310490+golovolom288@users.noreply.github.com>
2025-03-01 20:31:27 +0600

    /

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -20,9 +20,6 @@ from main import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('admin/', admin.site.urls),
-    path('product/<slug:slug>/', views.product_detail, name='product_detail'),
-    path('<slug:parent_slug>/<slug:slug>/', views.category_products, name='category_products'),
-    path('<slug:slug>/', views.category_products, name='category_products'),
     path('product_detail/<int:pk>/', views.product_detail, name='product_detail'),
     path('catalog/', views.catalog, name='catalog'),
     path('user/', include('user.urls')),
